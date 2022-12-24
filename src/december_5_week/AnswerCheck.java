44e73777207c1f6df766c27ca9909299c5aa67d6
김동민 <68542404+donggada@users.noreply.github.com>
2022-12-24 18:09:39 +0900

    advencd 문제 셋팅

--- src/december_5_week/AnswerCheck.java
+++ src/december_5_week/AnswerCheck.java
@@ -19,5 +19,19 @@ public class AnswerCheck {
         System.out.println("두 큐 합 같게 만들기 case 2 = " + solution1.solution(queue2_1, queue2_2));
         System.out.println("두 큐 합 같게 만들기 case 3 = " + solution1.solution(queue3_1, queue3_2));
 
+        december_5_week.advenced.Solution solution2 = new december_5_week.advenced.Solution();
+
+        int[] info1 = {0 ,0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1};
+        int[][] edges1 = {{0,1}, {1,2}, {1,4}, {0,8}, {8,7}, {9,10}, {9,11}, {4,3}, {6,5}, {4,6}, {8,9}};
+
+        int[] info2 = {0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0};
+        int[][] edges2 = {{0,1}, {0,2}, {1,3}, {1,4}, {2,5}, {2,6}, {3,7}, {4,8}, {6,9}, {9,10}};
+
+        System.out.println("양과 늑대 case 1 = " + solution2.solution(info1, edges1));
+        System.out.println("양과 늑대 case 2 = " + solution2.solution(info2, edges2));
+
+
+
+
     }
 }
---
+++ src/december_5_week/advenced/Solution.java
@@ -0,0 +1,10 @@
+package december_5_week.advenced;
+
+public class Solution {
+    public int solution(int[] info, int[][] edges) {
+        int answer = 0;
+
+        
+        return answer;
+    }
+}

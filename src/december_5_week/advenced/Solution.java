8e2fd122bb5c0503ac6adc17c986e6fea1d225ec
김동민 <68542404+donggada@users.noreply.github.com>
2023-01-06 23:57:04 +0900

    양과 늑대 풀이
    해준님 감사합니다

--- src/december_5_week/advenced/Solution.java
+++ src/december_5_week/advenced/Solution.java
@@ -1,10 +1,53 @@
 package december_5_week.advenced;
 
-public class Solution {
-    public int solution(int[] info, int[][] edges) {
-        int answer = 0;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-        
-        return answer;
+public class Solution {
+    static int result;
+    static HashMap<Integer, List<Integer>> nodeMap;
+    public int solution(int[] info, int[][] edges) {
+        result = 0;
+        nodeMap = new HashMap();
+        for (int[] e : edges) {
+            if (nodeMap.containsKey(e[0])) {
+                List<Integer> nodeList = nodeMap.get(e[0]);
+                nodeList.add(e[1]);
+            } else {
+                ArrayList<Integer> newNoneList = new ArrayList<>();
+                newNoneList.add(e[1]);
+                nodeMap.put(e[0], newNoneList);
+            }
+        }
+
+        DFS(info, 0, 0, 0, new ArrayList<>(););
+        return result;
+    }
+
+    private void DFS(int[] info,int node, int sheep, int wolf, List<Integer> nextNode) {
+        if (info[node] == 0) {
+            sheep++;
+        } else  {
+            wolf++;
+        }
+
+        if (wolf >= sheep) {
+            return;
+        }
+
+        result = Math.max(result, sheep);
+
+        List<Integer> next = new ArrayList<>();
+        next.addAll(nextNode);
+
+        if (nodeMap.containsKey(node)) {
+            next.addAll(nodeMap.get(node));
+        }
+        next.remove(Integer.valueOf(node));
+
+        for (int n : next) {
+            DFS(info, n, sheep, wolf, next);
+        }
     }
 }

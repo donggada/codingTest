4b79130ebe4ed3f7fd8ddfbe1bcb75ef42adbef2
김동민 <68542404+donggada@users.noreply.github.com>
2023-01-23 15:19:56 +0900

    1월 2주 기본문제 풀이

--- src/january_2_week/basic/Solution.java
+++ src/january_2_week/basic/Solution.java
@@ -1,44 +1,33 @@
 package january_2_week.basic;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Solution {
     public Integer[] solution(String today, String[] terms, String[] privacies) {
-        int[] answer = {};
-        int todayReplace = exchangeDateToInt(today);
+        LocalDate nowLocalDate = LocalDate.parse(today, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+
         ArrayList<Integer> resultList = new ArrayList<>();
         HashMap<String, Integer> termsMap = new HashMap<>();
         for (String term :terms) {
             String[] array = term.split(" ");
             termsMap.put(array[0], Integer.parseInt(array[1]));
         }
-
+        
         for (int i = 0; i < privacies.length ; i++) {
-            String[] array = privacies[i].split(" ");
-            String[] split = array[0].split("[.]");
+            String[] privacy = privacies[i].split(" ");
+            LocalDate localDate = LocalDate.parse(privacy[0], DateTimeFormatter.ofPattern("yyyy.MM.dd"));
 
-            int exchangeDateToInt = exchangeDateToInt(array[0]);
-
-            int month = Integer.parseInt(split[1]);
-            int sumMonth = month + termsMap.get(array[1]);
-
-            if (sumMonth / 13 == 0) {
-                exchangeDateToInt += termsMap.get(array[1]) * 100;
-            } else {
-                exchangeDateToInt += (sumMonth/13) * 10000;
-            }
-//            System.out.println("todayReplace = " + todayReplace);
-//            System.out.println("exchangeDateToInt = " + exchangeDateToInt);
-            if (todayReplace > exchangeDateToInt -1) {
+            if (!localDate.plusMonths(termsMap.get(privacy[1])).isAfter(nowLocalDate)) {
                 resultList.add(i + 1);
             }
         }
 
+
         return resultList.toArray(new Integer[0]);
     }
 
-    private int exchangeDateToInt (String date) {
-        return Integer.parseInt(date.replace(".", ""));
-    }
+
 }
